45aa087058cf281140c06da7ffbfafd22413410a
LUCKY-2004lgtm <luckykumawaat2004@gmail.com>
2025-05-14 17:16:07 +0530

    Add login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Login from './pages/Login'
+import Verify from './components/auth/Verify'
 
 const App = () => {
   return (
     <>
       <Login />
+  
     </>
   )
 }
---
+++ src/assets/images/projectImages.js
@@ -0,0 +1,193 @@
+
+
+// // lading page start//
+
+// import L1 from '../images/product1.png'
+// import L2 from '../images/product2.png'
+// import L3 from '../images/product3.png'
+// import L4 from '../images/product4.png'
+// import L5 from '../images/product5.png'
+// import L6 from '../images/product6.png'
+// import L7 from '../images/product7.png'
+// import L8 from '../images/product8.png'
+// import L9 from '../images/product9.png'
+// import L10 from '../images/product10.png'
+// import L11 from '../images/product11.png'
+// import L12 from '../images/product12.png'
+// import L13 from '../images/product13.png'
+// import L14 from '../images/product14.png'
+// import L15 from '../images/product15.png'
+// import L16 from '../images/product16.png'
+
+
+// import L17 from '../images/logo.png'
+// import L18 from '../images/search.png'
+// import L19 from '../images/person.png'
+// import L20 from '../images/shoppinbag.png'
+// import L21 from '../images/favorite.png'
+// import L22 from '../images/instagarm.png'
+// import L23 from '../images/facebook.png'
+// import L24 from '../images/pinterset.png'
+// import L25 from '../images/tiktok.png'
+// import L26 from '../images/herobanner.png'
+// import L27 from '../images/arrowforward.png'
+// import L28 from '../images/copyright.png'
+// import L29 from '../images/footerchatbox.png'
+// import L30 from '../images/arrowforward.png'
+// import L31 from '../images/Sustainability.png'
+
+// // landing page ends//
+
+// // Desktop/Landing/Collection Menu page start//
+// import C31 from '../images/product17.png'
+// import C32 from '../images/product18.png'
+
+// //Desktop/Landing/Collection Menu page end//
+
+
+// // Desktop/Landing/New in Menu page start//
+
+// import N33 from '../images/product19.png'
+// import N34 from '../images/product20.png'
+
+// // Desktop/Landing/New in Menu page end//
+
+// // Desktop/Landing/Plus Size Menu page start //
+
+// import PS35 from '../images/product21.png'
+// import PS36 from '../images/product22.png'
+// import PS37 from '../images/product23.png'
+
+// // Desktop/Landing/Plus Size Menu page end//
+
+// // Desktop/Landing/Sustainability Menu page start //
+// import SM38 from '../images/product24.png'
+// import SM39 from '../images/product25.png'
+// import SM40 from '../images/product26.png'
+// import SM41 from '../images/product27.png'
+// import SM42 from '../images/product28.png'
+// import SM43 from '../images/product29.png'
+
+// // Desktop/Landing/Sustainability Menu page end //
+
+// // Desktop/Search/02 page start //
+// import S44 from '../images/product30.png'
+// import S45 from '../images/product31.png'
+// import S46 from '../images/product32.png'
+// import S47 from '../images/product33.png'
+// import S48 from '../images/product34.png'
+// import S49 from '../images/product35.png'
+
+// // Desktop/Search/02 page end //
+
+// // filter page png //
+
+
+// // Desktop/Shop All/Filters/01  page start //
+// import F50 from '../images/filterbanner.png'
+
+// import F51 from '../images/product36.png'
+// import F52 from '../images/product37.png'
+// import F53 from '../images/product38.png'
+// import F54 from '../images/product39.png'
+// import F55 from '../images/product40.png'
+// import F56 from '../images/product41.png'
+
+// // Desktop/Shop All/Filters/01  page end //
+
+// // Desktop/Shop All/Filters/02 page start //
+// import F2_57 from '../images/product42.png'
+// import F2_58 from '../images/product43.png'
+// import F2_59 from '../images/product44.png'
+// // Desktop/Shop All/Filters/02 page end //
+
+// // Desktop/Product/Modiweek page start //
+// import M60 from '../images/product45.png'
+// import M61 from '../images/product46.png'
+// import M62 from '../images/product47.png'
+
+// //Desktop/Product/Modiweek page end //
+
+
+// //Plus Size/Filters/01  page start //
+
+// import PS63 from '../images/plussizefilterbanner1.png'
+// import PS64 from '../images/plussizefilterbanner2.png'
+
+// import PS65 from '../images/product48.png'
+// import PS66 from '../images/product49.png'
+// import PS67 from '../images/product50.png'
+// import PS68 from '../images/product51.png'
+// import PS69 from '../images/product52.png'
+// import PS70 from '../images/product53.png'
+
+// // Desktop/Plus Size/Filters/01  page end
+
+
+// // Desktop/Plus Size/Filters/02 page start //
+// import PS2_71 from '../images/product54.png'
+// import PS2_72 from '../images/product55.png'
+// import PS2_73 from '../images/product56.png'
+// import PS2_74 from '../images/product57.png'
+
+// // Desktop/Plus Size/Filters/02 page end //
+
+// // Desktop/Plus size Prouduct order pagr start //
+// import PO75 from '../images/product58.png'
+// import PO76 from '../images/product59.png'
+// import PO77 from '../images/product60.png'
+
+// // Desktop/Plus size Prouduct order pag end //
+
+// //Desktop/Sustainability/01 page start //
+
+// import SB78 from '../images/product61.png'
+// import SB79 from '../images/product62.png'
+// import SB80 from '../images/product63.png'
+// import SB81 from '../images/product64.png'
+
+// import SB82 from '../images/product65.png'
+// import SB83 from '../images/product66.png'
+// import SB84 from '../images/product67.png'
+// import SB85 from '../images/product68.png'
+// import SB86 from '../images/product69.png'
+// import SB87 from '../images/product70.png'
+// import SB88 from '../images/product71.png'
+
+// // Desktop/Sustainability/01 page end //
+
+// // Desktop/ Sustainability/02 page start //
+// import SB2_89 from '../images/product72.png'
+// import SB2_90 from '../images/product73.png'
+// import SB2_91 from '../images/product74.png'
+// import SB2_92 from '../images/product75.png'
+// import SB2_93 from '../images/product76.png'
+
+// //Desktop/ Sustainability/02 page end //
+
+
+
+// Desktop/Register/01  page start //
+import R94 from '../images/register.png'
+import R95 from '../images/apple.png'
+import R96 from '../images/facebookregister.png'
+import R97 from '../images/google.png'
+
+// Desktop/Register/01 page end //
+
+
+// R1-
+// SB-
+
+
+
+const IMAGES = {
+    RegisteImage: R94,
+    Apple: R95,
+    facebook: R96,
+    google: R97
+
+
+}
+
+export default IMAGES;
