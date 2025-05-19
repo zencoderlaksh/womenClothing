5e47f26b3d84f5ef512856f9cabdf2182aa8e854
Deepanshu9352 <sanskarisaini25@gmail.com>
2025-05-19 16:52:31 +0530

    changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import SearchProduct from "./component/Search/SearchProduct";
-import Register from "../Auth/Register";
-import Login from "../Auth/Login";
-import Filters from "../../components/Filters/Filters";
 
 const Home = () => {
   return (
-    <div>
-      <Register></Register>
-      <Login></Login>
-      {/* <Filters></Filters>  */}
-      <SearchProduct></SearchProduct>
-    </div>
+    <>
+
+    </>
   );
 };
 
