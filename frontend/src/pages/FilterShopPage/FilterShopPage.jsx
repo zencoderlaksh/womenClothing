e73e2a07b0d4f6b43f2ed49b85bb2136d5ad2e48
Deepanshu9352 <sanskarisaini25@gmail.com>
2025-06-12 09:52:19 +0530

    ui

--- frontend/src/pages/FilterShopPage/FilterShopPage.jsx
+++ frontend/src/pages/FilterShopPage/FilterShopPage.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import SearchCards from '../../components/SearchCart/SearchCards'
-import IMAGES from '../../assets/images/projectImage'
-import Filters from '../../components/Filters/Filters'
+import React, { useState } from 'react';
+import SearchCards from '../../components/SearchCart/SearchCards';
+import IMAGES from '../../assets/images/projectImage';
+import Filters from '../../components/Filters/Filters';
 
 const FilterShopPage = ({ button }) => {
     const DataProduct = [
@@ -13,7 +13,6 @@ const FilterShopPage = ({ button }) => {
             price: '$160',
             color: ['#909225'],
         },
-
         {
             id: 2,
             img: IMAGES.filterShopTwo,
@@ -22,7 +21,6 @@ const FilterShopPage = ({ button }) => {
             price: '$95',
             color: ['#0C0C0C', '#7DC3EB', '#748C70'],
         },
-
         {
             id: 3,
             img: IMAGES.filterShopThree,
@@ -31,7 +29,6 @@ const FilterShopPage = ({ button }) => {
             price: '$245',
             color: ['#0C0C0C', '#7DC3EB', '#748C70']
         },
-
         {
             id: 4,
             img: IMAGES.filterShopFour,
@@ -40,7 +37,6 @@ const FilterShopPage = ({ button }) => {
             price: '$199',
             color: ['#909225', '#CA6D29']
         },
-
         {
             id: 5,
             img: IMAGES.filterShopFive,
@@ -49,7 +45,6 @@ const FilterShopPage = ({ button }) => {
             price: '$180',
             color: ['#0C0C0C', '#19418E', '#748C70']
         },
-
         {
             id: 6,
             img: IMAGES.filterShopSix,
@@ -57,8 +52,79 @@ const FilterShopPage = ({ button }) => {
             content: 'Turn It Up Pullover',
             price: '$280',
             color: ['#0C0C0C', '#748C70']
-        }
-    ]
+        },
+        {
+            id: 7,
+            img: IMAGES.filterShopOne,
+            title: 'Winter Cardigan',
+            content: 'Turn It Up Knit',
+            price: '$140',
+            color: ['#403F3F', '#748C70']
+        },
+        {
+            id: 8,
+            img: IMAGES.filterShopTwo,
+            title: 'Everyday Tee',
+            content: 'Turn It Up Casual',
+            price: '$85',
+            color: ['#0C0C0C', '#D8BFD8']
+        },
+        {
+            id: 9,
+            img: IMAGES.filterShopThree,
+            title: 'Flowy Maxi Dress',
+            content: 'Turn It Up Maxi',
+            price: '$210',
+            color: ['#FFC0CB', '#748C70']
+        },
+        {
+            id: 10,
+            img: IMAGES.filterShopFour,
+            title: 'Fitted Denim Jacket',
+            content: 'Turn It Up Denim',
+            price: '$195',
+            color: ['#000080', '#708090']
+        },
+        {
+            id: 11,
+            img: IMAGES.filterShopFive,
+            title: 'Linen Shorts',
+            content: 'Turn It Up Shorts',
+            price: '$120',
+            color: ['#F5DEB3', '#0C0C0C']
+        },
+        {
+            id: 12,
+            img: IMAGES.filterShopSix,
+            title: 'Boss Blazer',
+            content: 'Turn It Up Formal',
+            price: '$305',
+            color: ['#0C0C0C', '#748C70']
+        },
+        {
+            id: 13,
+            img: IMAGES.filterShopFour,
+            title: 'Fitted Denim Jacket',
+            content: 'Turn It Up Denim',
+            price: '$195',
+            color: ['#000080', '#708090']
+        },
+        {
+            id: 14,
+            img: IMAGES.filterShopFive,
+            title: 'Linen Shorts',
+            content: 'Turn It Up Shorts',
+            price: '$120',
+            color: ['#F5DEB3', '#0C0C0C']
+        },
+    ];
+
+    const [visibleCount, setVisibleCount] = useState(6);
+
+    const handleLoadMore = () => {
+        setVisibleCount(prev => prev + 6);
+    };
+
     return (
         <>
             <div>
@@ -67,30 +133,48 @@ const FilterShopPage = ({ button }) => {
                 </div>
                 <div className='lg:flex'>
                     <div className="card-left bg-white ml-4 sm:ml-6 md:ml-59 lg:ml-18 mt-5 lg:w-[30%]">
-                        <Filters></Filters>
+                        <Filters />
                     </div>
                     <div className='card-data lg:mx-auto flex items-center justify-center gap-5 flex-wrap px-3'>
 
                         {
-                            DataProduct.map((item, i) => {
+                            DataProduct.slice(0, visibleCount).map((item, i) => {
                                 return (
-
-                                    <div className="card">
+                                    <div className="card" key={item.id}>
                                         <div className="card-right">
-                                            <SearchCards key={i} id={item.id} img={item.img} icon={item.i} title={item.title} content={item.content} price={item.price} color={item.color} />
+                                            <SearchCards
+                                                id={item.id}
+                                                img={item.img}
+                                                icon={item.i}
+                                                title={item.title}
+                                                content={item.content}
+                                                price={item.price}
+                                                color={item.color}
+                                            />
                                         </div>
                                     </div>
                                 )
                             })
                         }
-                        <div className='flex items-center justify-center'>
-                            <button className='w-[184px] py-2 mb-5 border-1 border-[#5A6D57] text-[#5A6D57] cursor-pointer'>{button}Load More</button>
-                        </div>
+
+                        {
+                            visibleCount < DataProduct.length && (
+                                <div className='flex items-center justify-center w-full'>
+                                    <button
+                                        onClick={handleLoadMore}
+                                        className='w-[184px] py-2 mb-5 border border-[#5A6D57] text-[#5A6D57] cursor-pointer'
+                                    >
+                                        {button} Load More
+                                    </button>
+                                </div>
+                            )
+                        }
+
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default FilterShopPage
+export default FilterShopPage;
--- frontend/src/pages/PlusSize/PlusSize.jsx
+++ frontend/src/pages/PlusSize/PlusSize.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 import SearchCards from '../../components/SearchCart/SearchCards'
 import IMAGES from '../../assets/images/projectImage'
 import Filters from '../../components/Filters/Filters'
 
-const PlusSize = ({button}) => {
-  const DataProduct = [
+const PlusSize = ({ button }) => {
+    const DataProduct = [
+        {
+            id: 1,
+            img: IMAGES.plusSizeOne,
+            title: 'Chill Dress',
+            content: 'Turn It Up Dress',
+            price: '$260',
+            color: ['#909225'],
+        },
+
+        {
+            id: 2,
+            img: IMAGES.plusSizeTwo,
+            title: 'Essential Dress',
+            content: 'Turn It Up Dress',
+            price: '$195',
+            color: ['#0C0C0C', '#7DC3EB', '##D0A5EA'],
+        },
+
+        {
+            id: 3,
+            img: IMAGES.plusSizeThree,
+            title: 'Casual Pants',
+            content: 'Turn It Up Pants',
+            price: '$85',
+            color: ['#909225', '#7DC3EB', '##CA6D29']
+        },
+
+        {
+            id: 4,
+            img: IMAGES.plusSizeFour,
+            title: 'Easy Skirt ',
+            content: 'Turn It Up Skirt',
+            price: '$109',
+            color: ['#909225', '#19418E']
+        },
+
+        {
+            id: 5,
+            img: IMAGES.plusSizeFive,
+            title: 'New Age Shirt',
+            content: 'Turn It Up Shirt',
+            price: '$80',
+            color: ['#0C0C0C', '#19418E', '#748C70']
+        },
+
+        {
+            id: 6,
+            img: IMAGES.plusSizeSix,
+            title: 'Boss Dress',
+            content: 'Turn It Up Dress',
+            price: '$280',
+            color: ['#0C0C0C', '#748C70']
+        },
         {
             id: 1,
             img: IMAGES.plusSizeOne,
@@ -59,20 +112,28 @@ const PlusSize = ({button}) => {
             color: ['#0C0C0C', '#748C70']
         }
     ]
-  return (
-    <>
-      <div>
+
+    const [visibleCount, setVisibleCount] = useState(6);
+
+    const handleLoadMore = () => {
+        setVisibleCount(prev => prev + 6);
+    };
+
+
+    return (
+        <>
+            <div>
                 <div>
                     <img className='mt-10 w-screen' src={IMAGES.plussizefilterbanner} alt="" />
                 </div>
                 <div className='lg:flex'>
                     <div className="card-left bg-white ml-4 sm:ml-6 md:ml-59 lg:ml-18 mt-5 lg:w-[30%]">
-                        <Filters></Filters>
+                        <Filters />
                     </div>
                     <div className='card-data lg:mx-auto flex items-center justify-center gap-5 flex-wrap px-3'>
 
                         {
-                            DataProduct.map((item, i) => {
+                            DataProduct.slice(0, visibleCount).map((item, i) => {
                                 return (
 
                                     <div className="card">
@@ -83,14 +144,23 @@ const PlusSize = ({button}) => {
                                 )
                             })
                         }
-                        <div className='flex items-center justify-center'>
-                            <button className='w-[184px] py-2 mb-5 border-1 border-[#5A6D57] text-[#5A6D57] cursor-pointer'>{button}Load More</button>
-                        </div>
+                        {
+                            visibleCount < DataProduct.length && (
+                                <div className='flex items-center justify-center w-full'>
+                                    <button
+                                        onClick={handleLoadMore}
+                                        className='w-[184px] py-2 mb-5 border border-[#5A6D57] text-[#5A6D57] cursor-pointer'
+                                    >
+                                        {button} Load More
+                                    </button>
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default PlusSize
